e92b11cdc3d5cc4066c06d2fd6262fbf3d3a1d27
curiousNEHA <155069649+curiousNEHA@users.noreply.github.com>
2024-07-02 05:19:57 +0000

    variable and datatype

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,12 @@
+"use strict";
+
+let person="Hitesh";
+let age=30;
+let isLoggedIn=false;
+
+console.log(person);
+console.log(age);
+console.log(isLoggedIn);
+
+console.log(typeof person);
+console.log(typeof null);
---
+++ 01_basics/variables.js
@@ -0,0 +1,5 @@
+let accountId=1124
+var accountName="NehaSharma"
+accountBalance=4000
+console.log(accountId)
+console.table([accountId,accountName,accountBalance])
